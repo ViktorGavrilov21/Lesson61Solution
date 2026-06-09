352fdc5b8f1e9d65b37f83450eb8e7cc683c49ec
Viktor Gavrilov <gavvikal0101@gmail.com>
2026-06-09 19:32:47 +0300

    add task07

--- Task01/test07.cpp
+++ Task01/test07.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 using namespace std;
 
+void init(int* array, int size) {
+	for (int i = 0; i < size; i++) {
+		*(array + i) = rand() % 10;
+	}
+}
+
 int main() {
-	int size;
+	int size = 10;
 
-	cout << "Input size: ";
-	cin >> size;
+	//cout << "Input size: ";
+	//cin >> size;
 
-	int* array = new int[size] {};
+	int array[10];
+
+	//int* array = new int[size] {};
+
+	init(array, size);
 
 	for (int i = 0; i < size; i++) {
-		cout << *(array + i) << " ";
+		//другой подход
+		cout << array[i] << " ";
 	}
 
-	delete[] array;
+	//delete[] array;
 
 
 	return 0;
