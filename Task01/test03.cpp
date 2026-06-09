c239b2dd11242c962233617c48abf55e20def0b4
Viktor Gavrilov <gavvikal0101@gmail.com>
2026-06-09 18:08:39 +0300

    add test03

---
+++ Task01/test03.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int array[3]{1, 2, 3};
+//
+//	cout << array << " - " << *array << endl;
+//	cout << &array[0] << " - " << array[0] << endl;
+//	cout << &array[1] << " - " << array[1] << endl;
+//	cout << &array[2] << " - " << array[2] << endl;
+//
+//
+//	return 0;
+//}
