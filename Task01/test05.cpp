6d71a30aae074ec5249b1f54e31c04c97557e6a7
Viktor Gavrilov <gavvikal0101@gmail.com>
2026-06-09 19:24:56 +0300

    add task05, 06

--- Task01/test05.cpp
+++ Task01/test05.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	const int SIZE = 5;
-	int array[SIZE]{ 1, 2, 3, 4, 5 };
-	
-	for (int i = 0; i < SIZE; i++) {
-		cout << (array + i) << " - " << *(array + i) << endl;
-	}
-
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	const int SIZE = 5;
+//	int array[SIZE]{ 1, 2, 3, 4, 5 };
+//	
+//	for (int i = 0; i < SIZE; i++) {
+//		cout << (array + i) << " - " << *(array + i) << endl;
+//	}
+//
+//
+//	return 0;
+//}
---
+++ Task01/test06.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int size;
+//
+//	cout << "Input size: ";
+//	cin >> size;
+//
+//	int* array = new int[size]{};
+//
+//	for (int i = 0; i < size; i++) {
+//		cout << *(array + i) << " ";
+//	}
+//
+//	delete[] array;
+//
+//
+//	return 0;
+//}
