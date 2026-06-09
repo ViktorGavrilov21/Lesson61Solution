35e46f54fea70e30480295b6dc2a532f0cf35ebd
Viktor Gavrilov <gavvikal0101@gmail.com>
2026-06-09 18:00:57 +0300

    Add project files.

---
+++ Task01/test01.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int* ptr = NULL;
+//	int number = 10;
+//
+//	ptr = &number;
+//
+//	*ptr *= 2;
+//
+//	cout << &number << " - " << number << endl;
+//	cout << &ptr << *ptr << endl;
+//
+//
+//	return 0;
+//}
